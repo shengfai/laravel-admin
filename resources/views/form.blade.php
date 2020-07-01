bbb8f2cad40169240ca95c491474b2842a3eb02b
盛辉 <shengfai@qq.com>
2020-07-01 13:51:02 +0800

    update 新增字段类型（number/date/datetime）

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -10,6 +10,8 @@
         <div class="layui-input-block">
         	@if($vo->getOption('type') === 'text')
             <input type="text" name="{{$field_name}}" value="{{$model->{$field_name} ?? ''}}" class="layui-input" required="required" title="请输入{{$title}}" placeholder="请输入{{$title}}">
+            @elseif($vo->getOption('type') === 'number')
+            <input type="number" name="{{$field_name}}" value="{{$model->{$field_name} ?? ''}}" class="layui-input" required="required" title="请输入{{$title}}" placeholder="请输入{{$title}}">
             @elseif($vo->getOption('type') === 'textarea')
             <textarea name="{{$field_name}}" class="layui-textarea" required="required" placeholder="请输入{{$title}}" title="请输入{{$title}}">{{$model->{$field_name} ?? ''}}</textarea>
             @elseif($vo->getOption('type') === 'switch')
@@ -18,6 +20,16 @@
             <input type="text" class="layui-input validate-error" required="required" onchange="$(this).nextAll('img').attr('src', this.value);" value="{{$model->{$field_name} ?? ''}}" name="{{$field_name}}" title="请上传图片或输入图片URL地址">
             <p class="help-block">{!! $vo->getOption('tips') !!}</p>
             <img style="width: 30px; height: auto;" data-tips-image="" src="{{$model->{$field_name} ?? '/admin/images/image.png'}}"> <a data-file="one" data-type="ico,png,jpeg,jpg" data-field="{{$field_name}}" class="btn btn-link">上传图片</a>
+            @elseif($vo->getOption('type') === 'datetime')
+            <input type="text" id="{{$field_name}}"  name="{{$field_name}}" value="{{$model->{$field_name} ?? ''}}" required="required" title="请选择{{$title}}" placeholder="请选择{{$title}}" class="layui-input">
+            <script type="text/javascript">
+            window.laydate.render({type: 'datetime', elem: '#{{$field_name}}', trigger: 'click', format: 'yyyy-MM-dd HH:mm:ss'});
+            </script>
+            @elseif($vo->getOption('type') === 'date')
+            <input type="text" id="{{$field_name}}"  name="{{$field_name}}" value="{{$model->{$field_name} ?? ''}}" required="required" title="请选择{{$title}}" placeholder="请选择{{$title}}" class="layui-input">
+            <script type="text/javascript">
+            window.laydate.render({type: 'date', elem: '#{{$field_name}}', trigger: 'click', format: 'yyyy-MM-dd'});
+            </script>
             @endif
         </div>
     </div>
