4df10b0b7dd8c54c6623bc9cd718cae23990d6f6
盛辉 <shengfai@qq.com>
2020-08-26 09:36:22 +0800

    update 添加标签关联控制台

---
+++ src/Controllers/TagsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Shengfai\LaravelAdmin\Controllers;
+
+use Illuminate\Http\Request;
+use Shengfai\LaravelAdmin\Models\Module;
+
+/**
+ * 标签控制器
+ * Class TagsController
+ *
+ * @package \Shengfai\LaravelAdmin\Controllers
+ * @author ShengFai <shengfai@qq.com>
+ */
+class TagsController extends Controller
+{
+
+    /**
+     * 设置关联标签
+     *
+     * @param Request $request
+     * @param string $class
+     * @param int $id
+     */
+    public function attach(Request $request, string $class, int $id)
+    {
+        // 当前模型
+        $model = Module::getModelByClassName($class);
+        
+        // 当前对象
+        $taggable = $model->namespace::find($id);
+        
+        if ($request->isMethod('Get')) {
+            return $this->view([
+                'id' => $id,
+                'class' => $class,
+                'model' => $model->load('dimensions'),
+                'used_tags' => $taggable->tags()->pluck('id')
+            ]);
+        }
+    
+        $taggable->tags()->sync($request->tags);
+        return $this->success();
+    }
+}
